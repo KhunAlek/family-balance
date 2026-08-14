19bd9ff62b0069609ab73983f9297be5d5b469c6
KhunAlek <abystrov66@gmail.com>
2026-08-14 08:56:34 +0700

    Fix v24.1 loader URL resolution

--- assets/v24/v24/v24_1_app4.js
+++ assets/v24/v24/v24_1_app4.js
@@ -1,1 +1,1 @@
-(()=>{const s=document.createElement('script');s.src='../v24_1_app4.js';s.async=false;document.head.appendChild(s)})();
+(()=>{const s=document.createElement('script');s.src='assets/v24/v24_1_app4.js';s.async=false;document.head.appendChild(s)})();
